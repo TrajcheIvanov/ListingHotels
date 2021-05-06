076781ae30aa7f5668a3fa921fb2f25ff1063c80
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-05-06 22:53:04 +0200

    Made Database

---
+++ ListingHotels/ListingHotels/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingHotels.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Counties { get; set; }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
--- ListingHotels/ListingHotels/Startup.cs
+++ ListingHotels/ListingHotels/Startup.cs
@@ -1,7 +1,9 @@
+using ListingHotels.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +28,11 @@ namespace ListingHotels
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
 
-            services.AddControllers();
+            
 
             services.AddCors(x => {
                 x.AddPolicy("AllowAll", builder =>
@@ -40,6 +45,8 @@ namespace ListingHotels
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ListingHotels", Version = "v1" });
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
